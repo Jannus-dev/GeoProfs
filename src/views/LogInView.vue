613ff6b3e13d3a15d581cd7e1187fffebdbe4a15
Jannus <jan.honing2@gmail.com>
2024-12-10 10:56:42 +0100

    Init commit

---
+++ src/views/LogInView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1 class="underline">Log In</h1>
+    <form>
+      <label for="email">Email</label>
+      <input type="email" id="email" name="email" required>
+      <label for="password">Password</label>
+      <input type="password" id="password" name="password" required>
+      <button type="submit">Log In</button>
+    </form>
+  </div>
+  <div class="astedicIMG">
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
